19a6102ba3f70636c7c983bb83f2c30da37a5895
Shaowen Zhu <shaowen@Shaowens-MacBook.local>
2022-09-25 23:15:03 +0800

    feat: add type validation for value field

--- models/humidity.js
+++ models/humidity.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 
 // versionKey: https://stackoverflow.com/a/31872302/16317008
 const humiditySchema = new mongoose.Schema({
-    value: Number,
+    value: {
+        type: Number,
+        max: [0, 'Humidity can not be negative'],
+        required: [true, 'Value field is required']
+    },
     create_date: Date
 }, { versionKey: false});
 
--- models/temperature.js
+++ models/temperature.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 
 // versionKey: https://stackoverflow.com/a/31872302/16317008
 const temperatureSchema = new mongoose.Schema({
-    value: Number,
+    value: {
+        type: Number,
+        max: [50, 'Temperature can not higher than 50'],
+        required: [true, 'Value field is required']
+    },
     create_date: Date
 }, { versionKey: false} );
 
